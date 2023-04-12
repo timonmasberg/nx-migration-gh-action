390867b12eb40beb7a6d8e8466209040f1db50eb
Timon Masberg <contact@timonmasberg.com>
2023-04-12 19:38:07 +0200

    Init

--- dist/index.js
+++ dist/index.js
@@ -12843,7 +12843,7 @@ FetchError.prototype.name = 'FetchError';
 
 let convert;
 try {
-	convert = (__nccwpck_require__(2877).convert);
+	convert = (__nccwpck_require__(3975).convert);
 } catch (e) {}
 
 const INTERNALS = Symbol('Body internals');
@@ -21469,14 +21469,6 @@ exports.getLatestNxVersion = getLatestNxVersion;
 
 /***/ }),
 
-/***/ 2877:
-/***/ ((module) => {
-
-module.exports = eval("require")("encoding");
-
-
-/***/ }),
-
 /***/ 9491:
 /***/ ((module) => {
 
@@ -21514,6 +21506,14 @@ module.exports = require("constants");
 
 "use strict";
 module.exports = require("crypto");
+
+/***/ }),
+
+/***/ 3975:
+/***/ ((module) => {
+
+"use strict";
+module.exports = require("encoding");
 
 /***/ }),
 
